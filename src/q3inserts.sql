c1edcb19c3af0a0aa715d34f111f3d843d1c5b34
gurshane <gurshane@ymail.com>
2015-03-07 18:30:20 -0500

    Updated q3inserts.sql and Q3 output
    -Added 5 INSERT statements to q3inserts.sql
    -Added output from these INSERT and SELECT statements to the doc

--- src/q3inserts.sql
+++ src/q3inserts.sql
@@ -1,9 +1,35 @@
-INSERT INTO pet_likes_interest VALUES(
-(6,1),
-(6,2),
-(8,3),
-(10,5),
-(10, 7)
+--COMP 421
+--Question 3
+--Inserts into one relation
+
+INSERT INTO users VALUES (
+	1,
+	'furrymachine@gmail.com',
+	'woofwoof123'
 );
 
-SELECT * FROM pet_likes_interest;
+INSERT INTO users VALUES (
+	2,
+	'phil@yahoo.com',
+	'password'
+);
+
+INSERT INTO users VALUES (
+	3,
+	'australia@outbackgrill.com',
+	'icantrememberthis321'
+);
+
+INSERT INTO users VALUES (
+	4,
+	'ihateinsertstatements@everyone.com',
+	'whyyyyyyY'
+);
+
+INSERT INTO users VALUES (
+	5,
+	'cat@daddy.com',
+	'purrrfect'
+);
+
+SELECT * FROM users;
