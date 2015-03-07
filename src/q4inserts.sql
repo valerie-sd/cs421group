b30f318a7641d06206652157d272e743d84c43d1
belphegors-prime <dblader79@gmail.com>
2015-03-06 20:18:04 -0500

    Update and rename src to src/q4inserts.sql

---
+++ src/q4inserts.sql
@@ -0,0 +1,10 @@
+INSERT INTO Users VALUES(
+(100, 'funluvndude@yahoo.com', 'alpha'),
+(101, 'ferretmaster247@gmail.com', 'beta'),
+(102, 'catmoviesrus@gmail.com', 'gamma'),
+(103, 'insanegamingdad@yahoo.com', 'delta'),
+(104, 'liketearsintherain@gmail.com', 'epsilon')
+);
+
+INSERT INTO Pages VALUES(
+);
