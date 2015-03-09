723ffd6295705d4122806f73e1a95614a73d6973
gurshane <gurshane@ymail.com>
2015-03-09 19:22:24 -0400

    Partial q5

--- src/q5queries.sql
+++ src/q5queries.sql
@@ -5,16 +5,10 @@
 
 --Returns all the posts made by a given page
 SELECT posts.post_id, posts.creator, pages.pname, pages.description, pages.manager_user_id
-FROM posts
-JOIN pages
-ON (posts.creator = pages.page_id);
+FROM posts, pages
+WHERE posts.creator = pages.page_id;
 
 --Returns the most popular interests amoungst users
-SELECT DISTINCT pages.pname, pages.description, interest_pages.page_id, interest_pages.num_likers
-FROM pages
-JOIN interest_pages
-ON interest_pages.num_likers >= 10;
-
 
 --Returns the more popular responses for each poll
 /*
