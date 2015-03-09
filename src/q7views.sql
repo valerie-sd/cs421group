8c62722c8f5f86fb177a9e2fb2dadce9ba177afc
belphegors-prime <dblader79@gmail.com>
2015-03-09 19:58:19 -0400

    Update q7views.sql

--- src/q7views.sql
+++ src/q7views.sql
@@ -9,4 +9,4 @@ WHERE PP.page_id = I.pet_id AND PA.page_id = I.pet_id AND I.interest_id = 17;
 CREATE VIEW NumberOfFriends AS
 SELECT DISTINCT pages.page_id, pages.pname, pet_pages.species, pet_pages.age, COUNT(*) FROM pages, pet_pages, pet_friends_with
 WHERE pages.page_id = pet_pages.page_id AND pages.page_id = pet_friends_with.pet_id OR pages.page_id = pet_friends_with.friend_id
-GROUP BY pages.page_id;
+GROUP BY pages.page_id, pages.pname, pet_pages.species, pet_pages.age;
