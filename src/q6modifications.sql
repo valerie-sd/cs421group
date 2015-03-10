701232e85a1413262029e06973020738fa106e8a
valerie-sd <audiographical@gmail.com>
2015-03-09 20:44:54 -0400

    These 3 should be correct

--- src/q6modifications.sql
+++ src/q6modifications.sql
@@ -5,24 +5,23 @@
 
 --alteration: for all pets with birthdays today, increment age by 1.
 UPDATE pet_pages SET age = age + 1
-	WHERE birthday == current_date;
+	WHERE birthday = current_date;
 
 --alteration: for all pets who answered yes to this poll, make a post saying so.
 	
 --alteration: delete a pet, and then decrement all pages that they liked.
-UPDATE interest_pages i 
-	INNER JOIN pet_likes_interest l ON
-		l.pet_id = 02 AND l.interest_id = i.page_id
-	SET num_likers -= 1;
+UPDATE interest_pages ip SET num_likers = num_likers - 1
+	FROM pet_likes_interest pli 
+	WHERE pli.pet_id = 02 AND pli.interest_id = ip.page_id;
 DELETE FROM pages WHERE page_id = 02;
 	 
 
 --alteration: add a "City" field to the pages table, and make it NULL-able. Add some values.
 ALTER TABLE pages 
 	ADD COLUMN city varchar(20) NULL;
-INSERT INTO pages (city) VALUES ("Montreal")
-	WHERE page_id % 3 == 2;
-INSERT INTO pages (city) VALUES ("Verdun")
-	WHERE page_id % 3 == 1;
-INSERT INTO pages (city) VALUES ("Westmount")
-	WHERE page_id % 3 == 0;
+UPDATE pages SET city = 'Montreal'
+	WHERE page_id % 3 = 2;
+UPDATE pages SET city = 'Verdun'
+	WHERE page_id % 3 = 1;
+UPDATE pages SET city = 'Westmount'
+	WHERE page_id % 3 = 0;
