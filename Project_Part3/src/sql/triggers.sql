245e791d2ed3220478af640d8203d63f86454d21
gurshane <gurshane@ymail.com>
2015-03-22 02:05:53 -0400

    DB party ends early
    -I mostly read up on what triggers and stored procedures were. I will
    get back to these two tomorrow.

---
+++ Project_Part3/src/sql/triggers.sql
@@ -0,0 +1,20 @@
+--COMP 421
+--Group 11
+--Project Part 3
+--TRIGGERS
+
+CREATE TABLE IF NOT EXISTS pet_page_backup_table (int num);
+/*(
+	page_id		INTEGER		NOT NULL,
+	species		VARCHAR(100)	NOT NULL,
+	gender		CHAR(1)		NOT NULL,
+	age		INTEGER,
+	birthday	DATE
+);
+*/
+/*
+CREATE  TRIGGER pet_page_backup BEFORE UPDATE
+ON pet_pages
+[
+-- Trigger logic goes here....
+];*/
