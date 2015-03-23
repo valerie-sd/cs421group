0151d8b4f19ad44f55e29eff71f4e194606cb054
gurshane <gurshane@ymail.com>
2015-03-22 23:07:57 -0400

    Triggers completed and output documented

--- Project_Part3/src/sql/triggers.sql
+++ Project_Part3/src/sql/triggers.sql
@@ -3,18 +3,32 @@
 --Project Part 3
 --TRIGGERS
 
-CREATE TABLE IF NOT EXISTS pet_page_backup_table (int num);
-/*(
+--Table to hold old page_id entries
+CREATE TABLE pet_page_backup_table (
 	page_id		INTEGER		NOT NULL,
 	species		VARCHAR(100)	NOT NULL,
 	gender		CHAR(1)		NOT NULL,
 	age		INTEGER,
 	birthday	DATE
 );
-*/
-/*
-CREATE  TRIGGER pet_page_backup BEFORE UPDATE
-ON pet_pages
-[
--- Trigger logic goes here....
-];*/
+
+--Keep track of any changes to pet_pages for future retrieval or comparison
+CREATE OR REPLACE FUNCTION keepBackup() RETURNS trigger AS $pet_page_backup$
+	BEGIN
+		IF NEW.page_id <> OLD.page_id OR NEW.species <> OLD.species OR NEW.gender <> OLD.gender OR NEW.age <> OLD.age OR NEW.birthday <> OLD.birthday THEN
+			INSERT INTO pet_page_backup_table 
+			VALUES (NEW.page_id, NEW.species, NEW.gender, NEW.age, NEW.birthday);
+		END IF;
+		RETURN NEW;
+	END;
+	
+$pet_page_backup$ LANGUAGE plpgsql;
+
+CREATE TRIGGER pet_page_backup BEFORE UPDATE ON pet_pages
+	FOR EACH ROW EXECUTE PROCEDURE keepBackup();
+
+--Triggers trigger
+UPDATE pet_pages SET species = 'Donkhay' WHERE page_id = 1;
+ 
+ --Does not trigger trigger
+UPDATE pet_pages SET species = 'Dog' WHERE page_id = 2;
